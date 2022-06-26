067e022b028f6fbda8b063f7a494335434c7e2f5
Dani BL <danibachsl@protonmail.com>
2022-06-26 12:17:48 +0200

    .

--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const Book = require('../models/Book.model');
+const Movie = require('../models/Movie.model');
 
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/library-project';
 
